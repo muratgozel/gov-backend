9c4d160d4ea9965be511540f2830c2d9bd03dfd0
Murat Gözel <murat@gozel.com.tr>
2022-05-17 11:32:53 +0200

    error handling added.

--- nodes/session/index.js
+++ nodes/session/index.js
@@ -1,28 +1,39 @@
 const {sql} = require('slonik')
 
 async function sessionInfo(token, request) {
+  const initialValue = {
+    method: {},
+    user: {}
+  }
   const exists = await request.pgpool.exists(sql`select id from sessions where token=${token}`)
 
   if (!exists) {
-    return {}
+    return initialValue
   }
 
-  const session = await request.pgconn.one(sql`select * from sessions where token=${token}`)
-  const method = await request.pgconn.one(sql`select * from signin_methods where id=${session.signin_method_id}`)
-  const user = await request.pgconn.one(sql`select * from users where id=${session.user_id}`)
-
-  return {
-    revoked: session.revoked,
-    method: {
-      name: method.name,
-      additional_params: method.additional_params
-    },
-    user: {
-      email: user.email,
-      phone_num: user.phone_num,
-      phone_num_country: user.phone_num_country
-    }
+  try {
+    const session = await request.pgconn.one(sql`select * from sessions where token=${token}`)
+    const method = await request.pgconn.one(sql`select * from signin_methods where id=${session.signin_method_id}`)
+    const user = await request.pgconn.one(sql`select * from users where id=${session.user_id}`)
+    
+    return {
+      revoked: session.revoked,
+      method: {
+        name: method.name,
+        additional_params: method.additional_params
+      },
+      user: {
+        email: user.email,
+        phone_num: user.phone_num,
+        phone_num_country: user.phone_num_country
+      }
+    }  
+  } catch (error) {
+    console.log(error)
+    return initialValue
   }
+
+  return initialValue
 }
 
 module.exports = {
